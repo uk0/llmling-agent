698e72047d3403b65d472367b38c20d27e418675
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-12-05 18:15:19 +0100

    chore: example prep

--- src/llmling_agent/config_resources/__init__.py
+++ src/llmling_agent/config_resources/__init__.py
@@ -8,6 +8,9 @@ from typing import Final
 OPEN_BROWSER: Final[str] = str(
     importlib.resources.files("llmling_agent.config_resources") / "open_browser.yml"
 )
+SUMMARIZE_README: Final[str] = str(
+    importlib.resources.files("llmling_agent.config_resources") / "summarize_readme.yml"
+)
 
 
-__all__ = ["OPEN_BROWSER"]
+__all__ = ["OPEN_BROWSER", "SUMMARIZE_README"]
