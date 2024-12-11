2da57b8988681b817cf3974dc204e66d9c67b567
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-12-11 18:47:24 +0100

    Bump version 0.3.0 → 0.3.1

--- src/llmling_agent/__init__.py
+++ src/llmling_agent/__init__.py
@@ -5,7 +5,7 @@ from llmling_agent.agent import LLMlingAgent
 from llmling_agent.runners import AgentOrchestrator, AgentRunConfig, SingleAgentRunner
 
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __all__ = [
     "AgentDefinition",
