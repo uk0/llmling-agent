acc8610e4f955db2ba78b5a2ec4323e05d3dfd80
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-12-13 21:39:17 +0100

    chore: register tools

--- src/llmling_agent/commands/builtin/__init__.py
+++ src/llmling_agent/commands/builtin/__init__.py
@@ -5,6 +5,14 @@ from llmling_agent.commands.builtin.hello import hello_command
 from llmling_agent.commands.builtin.help_cmd import help_cmd
 from llmling_agent.commands.builtin.prompts import list_prompts_cmd, prompt_cmd
 from llmling_agent.commands.builtin.agents import list_agents_cmd, switch_agent_cmd
+from llmling_agent.commands.builtin.session import clear_cmd, reset_cmd
+from llmling_agent.commands.builtin.utils import copy_clipboard_cmd, edit_env_cmd
+from llmling_agent.commands.builtin.tools import (
+    list_tools_cmd,
+    tool_info_cmd,
+    enable_tool_cmd,
+    disable_tool_cmd,
+)
 
 
 def get_builtin_commands() -> list[BaseCommand]:
@@ -16,4 +24,12 @@ def get_builtin_commands() -> list[BaseCommand]:
         prompt_cmd,
         switch_agent_cmd,
         list_agents_cmd,
+        clear_cmd,
+        reset_cmd,
+        copy_clipboard_cmd,
+        edit_env_cmd,
+        list_tools_cmd,
+        tool_info_cmd,
+        enable_tool_cmd,
+        disable_tool_cmd,
     ]
