19c78fe4af86c5b69ba5a043a18bdeef5d391d5f
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-12-14 07:19:32 +0100

    feat: dotenv support

--- src/llmling_agent/__init__.py
+++ src/llmling_agent/__init__.py
@@ -3,9 +3,11 @@
 from llmling_agent.models import AgentsManifest, SystemPrompt
 from llmling_agent.agent import LLMlingAgent
 from llmling_agent.runners import AgentOrchestrator, AgentRunConfig, SingleAgentRunner
-
+from dotenv import load_dotenv
 
 __version__ = "0.5.0"
+
+load_dotenv()
 
 __all__ = [
     "AgentOrchestrator",
