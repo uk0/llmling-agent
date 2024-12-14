b0e5ded2ab22775d0c98f5b9223745dbabbdf609
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-12-14 07:20:31 +0100

    Bump version 0.5.0 → 0.5.1

--- src/llmling_agent/__init__.py
+++ src/llmling_agent/__init__.py
@@ -5,7 +5,7 @@ from llmling_agent.agent import LLMlingAgent
 from llmling_agent.runners import AgentOrchestrator, AgentRunConfig, SingleAgentRunner
 from dotenv import load_dotenv
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 load_dotenv()
 
