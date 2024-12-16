0d4fde30a3c9c825f31656c2a5f68ab676929b7b
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-12-16 03:12:02 +0100

    chore: export functional to top level module

--- src/llmling_agent/__init__.py
+++ src/llmling_agent/__init__.py
@@ -3,6 +3,12 @@
 from llmling_agent.models import AgentsManifest, SystemPrompt
 from llmling_agent.agent import LLMlingAgent
 from llmling_agent.runners import AgentOrchestrator, AgentRunConfig, SingleAgentRunner
+from llmling_agent.functional import (
+    run_with_model,
+    run_with_model_sync,
+    run_agent_pipeline_sync,
+    run_agent_pipeline,
+)
 from dotenv import load_dotenv
 
 __version__ = "0.6.0"
@@ -16,4 +22,8 @@ __all__ = [
     "LLMlingAgent",
     "SingleAgentRunner",
     "SystemPrompt",
+    "run_agent_pipeline",
+    "run_agent_pipeline_sync",
+    "run_with_model",
+    "run_with_model_sync",
 ]
