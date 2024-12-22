39073c897ce8446b0efe7fb5ae62fcebbb8ae5bf
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-12-22 09:53:52 +0100

    chore: top level imports

--- src/llmling_agent/__init__.py
+++ src/llmling_agent/__init__.py
@@ -2,13 +2,13 @@
 
 from llmling_agent.models import AgentsManifest, SystemPrompt
 from llmling_agent.agent import LLMlingAgent
-from llmling_agent.runners import AgentOrchestrator, AgentRunConfig, SingleAgentRunner
 from llmling_agent.functional import (
     run_with_model,
     run_with_model_sync,
     run_agent_pipeline_sync,
     run_agent_pipeline,
 )
+from llmling_agent.delegation.pool import AgentPool
 from dotenv import load_dotenv
 
 __version__ = "0.12.1"
@@ -16,11 +16,9 @@ __version__ = "0.12.1"
 load_dotenv()
 
 __all__ = [
-    "AgentOrchestrator",
-    "AgentRunConfig",
+    "AgentPool",
     "AgentsManifest",
     "LLMlingAgent",
-    "SingleAgentRunner",
     "SystemPrompt",
     "run_agent_pipeline",
     "run_agent_pipeline_sync",
