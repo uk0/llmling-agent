7ead07fd9640fb3081e9a056150c0113a9983d9d
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-12-29 04:16:05 +0100

    Bump version 0.13.0 → 0.13.1

--- src/llmling_agent/__init__.py
+++ src/llmling_agent/__init__.py
@@ -12,7 +12,7 @@ from llmling_agent.functional import (
 from llmling_agent.delegation.pool import AgentPool
 from dotenv import load_dotenv
 
-__version__ = "0.13.0"
+__version__ = "0.13.1"
 
 load_dotenv()
 
