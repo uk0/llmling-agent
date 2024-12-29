12c08b1bc39a0afdcf579a6b535d48d36b35814e
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-12-29 22:00:53 +0100

    chore: cleanup

--- src/llmling_agent/ui/interfaces.py
+++ src/llmling_agent/ui/interfaces.py
@@ -2,11 +2,7 @@
 
 from __future__ import annotations
 
-from typing import (
-    TYPE_CHECKING,
-    Protocol,
-    runtime_checkable,
-)
+from typing import TYPE_CHECKING, Protocol, runtime_checkable
 
 
 if TYPE_CHECKING:
