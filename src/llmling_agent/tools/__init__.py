4b039b7c2e69663fc7255aca218520312eeebc43
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2024-12-31 07:05:07 +0100

    chore: toolinfo to tools.__init__

--- src/llmling_agent/tools/__init__.py
+++ src/llmling_agent/tools/__init__.py
@@ -2,14 +2,9 @@
 
 from __future__ import annotations
 
-from llmling_agent.tools.base import ToolContext
+from llmling_agent.tools.base import ToolContext, ToolInfo
 from llmling_agent.tools.history import HistoryTools
 from llmling_agent.tools.manager import ToolManager, ToolError
 
 
-__all__ = [
-    "HistoryTools",
-    "ToolContext",
-    "ToolError",
-    "ToolManager",
-]
+__all__ = ["HistoryTools", "ToolContext", "ToolError", "ToolInfo", "ToolManager"]
