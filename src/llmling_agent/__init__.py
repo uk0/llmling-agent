cac830b8ef91e2772f0c18270a1de42028fffc17
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-01-01 14:37:32 +0100

    Bump version 0.15.1 → 0.16.0

--- src/llmling_agent/__init__.py
+++ src/llmling_agent/__init__.py
@@ -14,7 +14,7 @@ from dotenv import load_dotenv
 from llmling_agent.models.messages import ChatMessage
 from llmling_agent.chat_session.base import AgentPoolView
 
-__version__ = "0.15.1"
+__version__ = "0.16.0"
 
 load_dotenv()
 
