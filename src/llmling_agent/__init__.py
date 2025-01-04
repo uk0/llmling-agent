85f9c03f7310d058805022de1338d7deb4039e3a
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-01-04 08:07:41 +0100

    chore: add stuff to top level __init__

--- src/llmling_agent/__init__.py
+++ src/llmling_agent/__init__.py
@@ -1,7 +1,7 @@
 """Agent configuration and creation."""
 
 from llmling_agent.models import AgentsManifest, SystemPrompt, AgentConfig
-from llmling_agent.agent import Agent
+from llmling_agent.agent import Agent, HumanAgent
 from llmling_agent.functional import (
     run_with_model,
     run_with_model_sync,
@@ -9,7 +9,16 @@ from llmling_agent.functional import (
     run_agent_pipeline,
     get_structured,
 )
-from llmling_agent.delegation.pool import AgentPool
+from llmling_agent.delegation import (
+    AgentPool,
+    ConversationController,
+    Decision,
+    EndDecision,
+    RouteDecision,
+    TalkBackDecision,
+    interactive_controller,
+    rule_based_controller,
+)
 from dotenv import load_dotenv
 from llmling_agent.models.messages import ChatMessage
 from llmling_agent.chat_session.base import AgentPoolView
@@ -25,8 +34,16 @@ __all__ = [
     "AgentPoolView",
     "AgentsManifest",
     "ChatMessage",
+    "ConversationController",
+    "Decision",
+    "EndDecision",
+    "HumanAgent",
+    "RouteDecision",
     "SystemPrompt",
+    "TalkBackDecision",
     "get_structured",
+    "interactive_controller",
+    "rule_based_controller",
     "run_agent_pipeline",
     "run_agent_pipeline_sync",
     "run_with_model",
