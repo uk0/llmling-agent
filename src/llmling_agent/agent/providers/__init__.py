a5624d1ede580c144276a142716a7bf0f6f3371a
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-01-06 08:34:54 +0100

    chore: cleanup

--- src/llmling_agent/agent/providers/__init__.py
+++ src/llmling_agent/agent/providers/__init__.py
@@ -2,16 +2,9 @@
 
 from __future__ import annotations
 
-from typing import TypeVar
-
-from llmling_agent.agent.providers.base import (
-    AgentProvider,
-    ProviderResponse,
-)
+from llmling_agent.agent.providers.base import AgentProvider, ProviderResponse
 from llmling_agent.agent.providers.human import HumanProvider
 from llmling_agent.agent.providers.pydanticai import PydanticAIProvider
-
-TDeps = TypeVar("TDeps")
 
 type AnyProvider = PydanticAIProvider | HumanProvider
 
