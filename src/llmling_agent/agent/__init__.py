8c6ae4c1427b6c21d6b16809ec50342bdd864aa7
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-01-15 05:02:12 +0100

    chore: correct type order

--- src/llmling_agent/agent/__init__.py
+++ src/llmling_agent/agent/__init__.py
@@ -6,16 +6,16 @@ from typing_extensions import TypeVar
 from llmling_agent.agent.agent import Agent
 from llmling_agent.agent.structured import StructuredAgent
 
+
+TDeps = TypeVar("TDeps")
+TResult = TypeVar("TResult", default=str)
+
 type AnyAgent[TDeps, TResult] = Agent[TDeps] | StructuredAgent[TDeps, TResult]
 
 from llmling_agent.agent.agent_logger import AgentLogger
 from llmling_agent.agent.conversation import ConversationManager
 from llmling_agent.agent.container import AgentContainer
 from llmling_agent.agent.talk import Interactions
-
-
-TDeps = TypeVar("TDeps")
-TResult = TypeVar("TResult", default=str)
 
 
 __all__ = [
