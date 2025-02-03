5ec8a91c88ded05e43221404e704892f48b87c31
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-02-03 15:05:45 +0100

    chore: add BaseTeam to delegation package

--- src/llmling_agent/delegation/__init__.py
+++ src/llmling_agent/delegation/__init__.py
@@ -1,6 +1,7 @@
 """Agent delegation and collaboration functionality."""
 
 from llmling_agent.delegation.pool import AgentPool
+from llmling_agent.delegation.base_team import BaseTeam
 from llmling_agent.delegation.teamrun import TeamRun
 from llmling_agent.delegation.injection import AgentInjectionError, inject_agents
 from llmling_agent.delegation.decorators import with_agents
@@ -9,6 +10,7 @@ from llmling_agent.delegation.team import Team
 __all__ = [
     "AgentInjectionError",
     "AgentPool",
+    "BaseTeam",
     "Team",
     "TeamRun",
     "inject_agents",
