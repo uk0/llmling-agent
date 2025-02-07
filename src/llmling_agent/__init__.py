95c562891ae2385b8f9dd9726fb959a6325af644
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-02-07 20:56:12 +0100

    Bump version 0.99.0 → 0.99.1

--- src/llmling_agent/__init__.py
+++ src/llmling_agent/__init__.py
@@ -7,7 +7,7 @@ from llmling_agent.delegation import AgentPool, Team, TeamRun
 from dotenv import load_dotenv
 from llmling_agent.messaging.messages import ChatMessage
 
-__version__ = "0.99.0"
+__version__ = "0.99.1"
 
 load_dotenv()
 
