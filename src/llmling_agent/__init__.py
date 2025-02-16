e712f445ea879765b49e8341b32af553cfa5f6a5
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-02-17 00:12:09 +0100

    Bump version 0.99.12 → 0.99.13

--- src/llmling_agent/__init__.py
+++ src/llmling_agent/__init__.py
@@ -9,7 +9,7 @@ from llmling_agent.messaging.messages import ChatMessage
 from llmling_agent.tools import Tool, ToolCallInfo
 from llmling_agent.messaging.messagenode import MessageNode
 
-__version__ = "0.99.12"
+__version__ = "0.99.13"
 
 load_dotenv()
 
