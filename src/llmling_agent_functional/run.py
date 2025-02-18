6804db4c6a0be85ea0409b37ac469750d2af273f
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-02-18 07:19:34 +0100

    chore: image_url kwarg for functional calls

--- src/llmling_agent_functional/run.py
+++ src/llmling_agent_functional/run.py
@@ -8,6 +8,7 @@ from typing import TYPE_CHECKING, Any, Unpack, overload
 from typing_extensions import TypeVar
 
 from llmling_agent import Agent
+from llmling_agent_config.content import ImageURLContent
 
 
 if TYPE_CHECKING:
@@ -25,6 +26,7 @@ TResult = TypeVar("TResult", default=str)
 @overload
 async def run_agent[TResult](
     prompt: AnyPromptType | PIL.Image.Image | os.PathLike[str],
+    image_url: str | None = None,
     *,
     result_type: type[TResult],
     **kwargs: Unpack[AgentKwargs],
@@ -34,12 +36,14 @@ async def run_agent[TResult](
 @overload
 async def run_agent(
     prompt: AnyPromptType | PIL.Image.Image | os.PathLike[str],
+    image_url: str | None = None,
     **kwargs: Unpack[AgentKwargs],
 ) -> str: ...
 
 
 async def run_agent(
     prompt: AnyPromptType | PIL.Image.Image | os.PathLike[str],
+    image_url: str | None = None,
     *,
     result_type: type[Any] | None = None,
     **kwargs: Unpack[AgentKwargs],
@@ -47,13 +51,18 @@ async def run_agent(
     """Run prompt through agent and return result."""
     agent = Agent[None](**kwargs)
     async with agent:
-        result = await agent.run(prompt, result_type=result_type)
+        if image_url:
+            image = ImageURLContent(url=image_url)
+            result = await agent.run(prompt, image, result_type=result_type)
+        else:
+            result = await agent.run(prompt, result_type=result_type)
         return result.content
 
 
 @overload
 def run_agent_sync[TResult](
     prompt: AnyPromptType | PIL.Image.Image | os.PathLike[str],
+    image_url: str | None = None,
     *,
     result_type: type[TResult],
     **kwargs: Unpack[AgentKwargs],
@@ -63,21 +72,18 @@ def run_agent_sync[TResult](
 @overload
 def run_agent_sync(
     prompt: AnyPromptType | PIL.Image.Image | os.PathLike[str],
+    image_url: str | None = None,
     **kwargs: Unpack[AgentKwargs],
 ) -> str: ...
 
 
 def run_agent_sync(
     prompt: AnyPromptType | PIL.Image.Image | os.PathLike[str],
+    image_url: str | None = None,
     *,
     result_type: type[Any] | None = None,
     **kwargs: Unpack[AgentKwargs],
 ) -> Any:
     """Sync wrapper for run_agent."""
-    return asyncio.run(
-        run_agent(
-            prompt,
-            result_type=result_type,  # type: ignore
-            **kwargs,
-        )
-    )
+    coro = run_agent(prompt, image_url, result_type=result_type, **kwargs)  # type: ignore
+    return asyncio.run(coro)
