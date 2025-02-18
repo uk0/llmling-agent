03b7f41354071230358982708c87110599f65d6d
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-02-18 07:37:20 +0100

    chore: expose more methods to jinjarope

--- src/llmling_agent/utils/__init__.py
+++ src/llmling_agent/utils/__init__.py
@@ -14,7 +14,19 @@ def setup_env(env: jinja2.Environment):
         env: The jinjarope environment to extend
     """
     from llmling_agent.agent.agent import Agent
-    from llmling_agent_functional import run_agent, run_agent_sync
+    from llmling_agent_functional import (
+        run_agent,
+        run_agent_sync,
+        get_structured,
+        get_structured_multiple,
+        pick_one,
+    )
 
     env.globals |= dict(agent=Agent)
-    env.filters |= {"run_agent": run_agent, "run_agent_sync": run_agent_sync}
+    env.filters |= {
+        "run_agent": run_agent,
+        "run_agent_sync": run_agent_sync,
+        "pick_one": pick_one,
+        "get_structured": get_structured,
+        "get_structured_multiple": get_structured_multiple,
+    }
