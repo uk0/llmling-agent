748d9e157e82e08b97dfcd8a768d27de40a43ac1
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-09-26 01:22:47 +0200

    chore: cleanup

--- src/llmling_agent_acp/__init__.py
+++ src/llmling_agent_acp/__init__.py
@@ -2,10 +2,8 @@
 
 from __future__ import annotations
 
-from acp import DefaultACPClient
 from llmling_agent_acp.server import ACPServer
 from llmling_agent_acp.acp_agent import LLMlingACPAgent
-from llmling_agent_acp.wrappers import ACPClientInterface
 from llmling_agent_acp.session import ACPSession, ACPSessionManager
 from llmling_agent_acp.command_bridge import ACPCommandBridge
 from llmling_agent_acp.converters import (
@@ -16,20 +14,16 @@ from llmling_agent_acp.converters import (
     to_session_updates,
 )
 from llmling_agent_acp.tools import ACPToolBridge
-from acp.acp_types import StopReason
 
 
 __all__ = [
-    "ACPClientInterface",
     "ACPCommandBridge",
     "ACPServer",
     "ACPSession",
     "ACPSessionManager",
     "ACPToolBridge",
-    "DefaultACPClient",
     "FileSystemBridge",
     "LLMlingACPAgent",
-    "StopReason",
     "convert_acp_mcp_server_to_config",
     "from_content_blocks",
     "to_content_blocks",
--- src/llmling_agent_acp/wrappers.py
+++
@@ -1,7 +0,0 @@
-"""Client interface aliases for ACP integration."""
-
-from acp import Client
-
-
-# Backward compatibility alias
-ACPClientInterface = Client
